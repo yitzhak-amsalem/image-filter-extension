f9c9973aae8e3d5ba3e071ff0ace6212f9b01557
Yitzhak Amsalem <yitzhak@gmail.com>
2023-06-08 15:50:43 +0300

    send image array to server in base64

--- src/scripts/content.js
+++ src/scripts/content.js
@@ -38,20 +38,22 @@ function letsFilterImages(imageAlbumElement) {
     const imgElements = extractImageFiles(imageAlbumElement);
     console.log("images length:", imgElements.length)
     sendImageArrToService(imgElements);
-
-/*    forwardImage(imageAlbumElement);
-    downloadImage(img);*/
 }
 
-function sendImageArrToService(imageArr) {
-    const formData = new FormData();
-    Array.from(imageArr).forEach((image, index) => {
-        formData.append(`image_${index}`, imageToFile(image));
-    });
+async function sendImageArrToService(imageArr) {
+    /*   const formData = new FormData();
+       Array.from(imageArr).forEach((image, index) => {
+           formData.append(`image_${index}`, imageUrlToFile(image.src))
+       });*/
 
-    fetch('http://127.0.0.1:5000/upload', {
+//    imageUrlToFile(imageArr[0].src)
+    imageArrUrlToFiles(imageArr)
+    /*fetch('http://127.0.0.1:5000/upload', {
         method: 'POST',
-        body: formData
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({images: base64Images})
     }).then(response => {
         if (response.ok) {
             return response.text();
@@ -61,34 +63,72 @@ function sendImageArrToService(imageArr) {
     }).then(responseText => {
         console.log('Response:', responseText);
     }).catch(error => {
-            console.error('Error:', error);
-    });
+        console.error('Error:', error);
+    });*/
 
 //    const imageFile = imageToFile(image)
 //    formData.append('image', imageFile, 'image.png');
 }
 
-function extractImageFiles(imageAlbumElement) {
-    const imgElements =  Array.from(imageAlbumElement.querySelectorAll("img"));
-    return imgElements.filter((imgElement) => imgElement.src.includes("whatsapp"));
+function imageArrUrlToFiles(imageElements){
+    Promise.all(
+        imageElements.map((img, index) =>
+            fetch(img.src)
+                .then(response => response.arrayBuffer())
+                .then(buffer => {
+                    const base64Image = arrayBufferToBase64(buffer);
+                    return { index, image: base64Image };
+                })
+        )
+    ).then(imagesWithIndexes => {
+            fetch('http://127.0.0.1:5000/upload', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ images: imagesWithIndexes })
+            }).then(response => {
+                if (response.ok) {
+                    return response.text();
+                } else {
+                    throw new Error('Failed to send image');
+                }
+            }).then(responseText => {
+                console.log('Response:', responseText);
+            }).catch(error => {
+                console.error('Error:', error);
+            });
+    });
 }
 
-function imageToFile(image){
-    const canvas = document.createElement('canvas');
-    const context = canvas.getContext('2d');
-    canvas.width = image.width;
-    canvas.height = image.height;
-    context.drawImage(image, 0, 0);
+function imageUrlToFile(imageUrls) {
+    fetch(imageUrls)
+        .then(response => response.arrayBuffer())
+        .then(buffer => {
+            // Convert the ArrayBuffer to a Base64 string
+            const base64Image = arrayBufferToBase64(buffer);
 
-    const dataURL = canvas.toDataURL('image/png');
-    const imageData = dataURL.replace(/^data:image\/(png|jpeg);base64,/, '');
+            // Send the Base64-encoded image to the server using a POST request
+            fetch('http://127.0.0.1:5000/upload1', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ image: base64Image })
+            })
+                .then(response => {
+                    // Handle the response from the server
+                    console.log('Image uploaded successfully');
+                })
+                .catch(error => {
+                    console.error('Error uploading image:', error);
+                });
+        });
+}
 
-    const byteCharacters = atob(imageData);
-    const byteArrays = [];
-    for (let i = 0; i < byteCharacters.length; i++) {
-        byteArrays.push(byteCharacters.charCodeAt(i));
-    }
-    return new Blob([new Uint8Array(byteArrays)], {type: 'image/png'});
+function extractImageFiles(imageAlbumElement) {
+    const imgElements = Array.from(imageAlbumElement.querySelectorAll("img"));
+    return imgElements.filter((imgElement) => imgElement.src.includes("whatsapp"));
 }
 
 function createButtonElement(imageAlbumElement) {
@@ -99,9 +139,12 @@ function createButtonElement(imageAlbumElement) {
     filterButton.style.border = "3px solid green";
     filterButton.style.background = "#c9fc9f";
     filterButton.style.color = "#276001";
-    filterButton.style.height = "50px";
-    filterButton.style.width = "100px";
+    filterButton.style.height = "70px";
+    filterButton.style.width = "120px";
     filterButton.style.marginRight = "100px";
+    filterButton.style.fontFamily = 'Chivo Mono, monospace';
+    filterButton.style.borderRadius = "10px";
+    filterButton.style.fontSize = "1.5em";
 
     filterButton.addEventListener('mouseover', function () {
         this.style.transition = 'background-color 1s, color 0.5s';
@@ -116,7 +159,17 @@ function createButtonElement(imageAlbumElement) {
     return filterButton;
 }
 
-function downloadImage(image){
+function arrayBufferToBase64(buffer) {
+    let binary = '';
+    const bytes = new Uint8Array(buffer);
+    const len = bytes.byteLength;
+    for (let i = 0; i < len; i++) {
+        binary += String.fromCharCode(bytes[i]);
+    }
+    return window.btoa(binary);
+}
+
+function downloadImage(image) {
     const link = document.createElement('a');
     link.href = image.src;
     link.download = 'image.jpg';
@@ -139,3 +192,23 @@ function forwardImage(imageAlbumElement) {
         }, 1000)
     }
 }
+
+function imageToFile(image) {
+    const canvas = document.createElement('canvas');
+    const context = canvas.getContext('2d');
+    canvas.width = image.width;
+    canvas.height = image.height;
+    context.drawImage(image, 0, 0);
+
+    const dataURL = canvas.toDataURL('image/png');
+    const imageData = dataURL.replace(/^data:image\/(png|jpeg);base64,/, '');
+
+    const byteCharacters = atob(imageData);
+    const byteArrays = [];
+    for (let i = 0; i < byteCharacters.length; i++) {
+        byteArrays.push(byteCharacters.charCodeAt(i));
+    }
+    return new Blob([new Uint8Array(byteArrays)], {type: 'image/png'})
+}
+
+
