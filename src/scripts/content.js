57de412bdb46119c05b0e6e6d3961b7b8670ae4d
Yitzhak Amsalem <yitzhak@gmail.com>
2023-06-08 11:13:16 +0300

    send image array to server

--- src/scripts/content.js
+++ src/scripts/content.js
@@ -1,16 +1,3 @@
-function onNewChatOpened(conversationPanelWrapper) {
-    console.log("New chat opened!");
-    insertFilterButton(conversationPanelWrapper)
-}
-
-function insertFilterButton(conversationPanelWrapper) {
-    const imageAlbumElement = conversationPanelWrapper.querySelector('[data-testid="image-album"]');
-    const filterButton = createButtonElement(imageAlbumElement);
-    if (imageAlbumElement) {
-        imageAlbumElement.insertAdjacentElement("beforeend", filterButton)
-    }
-}
-
 const observer = new MutationObserver((mutationsList) => {
     for (let mutation of mutationsList) {
         if (mutation.type === "childList" && mutation.addedNodes.length > 0) {
@@ -29,66 +16,38 @@ const observer = new MutationObserver((mutationsList) => {
 const observerConfig = {childList: true, subtree: true};
 observer.observe(document.body, observerConfig);
 
+function onNewChatOpened(conversationPanelWrapper) {
+    console.log("New chat opened!");
+    insertFilterButton(conversationPanelWrapper)
+}
+
+function insertFilterButton(conversationPanelWrapper) {
+    const imageAlbumElement = conversationPanelWrapper.querySelector('[data-testid="image-album"]');
+    const filterButton = createButtonElement(imageAlbumElement);
+    if (imageAlbumElement) {
+        imageAlbumElement.insertAdjacentElement("beforeend", filterButton)
+    }
+}
+
 function onFilter(imageAlbumElement) {
     alert("Filter!!!")
-    letFilterImages(imageAlbumElement);
+    letsFilterImages(imageAlbumElement);
 }
 
-function letFilterImages(imageAlbumElement) {
-    console.log(imageAlbumElement)
-    const imgElements = imageAlbumElement.querySelectorAll("img");
-    imgElements.forEach((imgElement) => {
-        if (imgElement.src.includes("whatsapp")) {
-            console.log("Image source:", imgElement.src);
-        }
-    });
-    const link = document.createElement('a');
-    const img = imgElements[1];
-    link.href = img.src;
-    link.download = 'image.jpg';
-    //link.click();
+function letsFilterImages(imageAlbumElement) {
+    const imgElements = extractImageFiles(imageAlbumElement);
+    console.log("images length:", imgElements.length)
+    sendImageArrToService(imgElements);
 
-    const canvas = document.createElement('canvas');
-    const context = canvas.getContext('2d');
-
-    canvas.width = img.width;
-    canvas.height = img.height;
-
-    context.drawImage(img, 0, 0);
-
-    const dataURL = canvas.toDataURL('image/png'); // 'image/png' for PNG format, 'image/jpeg' for JPEG format
-
-    const imageData = dataURL.replace(/^data:image\/(png|jpeg);base64,/, '');
-
-    sendImageToService(imageData);
-
-    /*    const forward = imageAlbumElement.querySelector('[data-testid="forward-chat"]')
-    if (forward){
-        console.log(forward)
-        forward.click();
-        setTimeout(() => {
-            const popup = document.querySelector('[data-testid="chat-modal"]')
-            const element = document.querySelector('[data-testid="message-yourself-row"]');
-            element.click();
-        }, 1000)
-        setTimeout(() => {
-            const sendBtn = document.querySelector('[data-testid="send"]')
-            sendBtn.click();
-        }, 1000)
-    }
-*/
+/*    forwardImage(imageAlbumElement);
+    downloadImage(img);*/
 }
 
-function sendImageToService(image) {
-    const byteCharacters = atob(image);
-    const byteArrays = [];
-    for (let i = 0; i < byteCharacters.length; i++) {
-        byteArrays.push(byteCharacters.charCodeAt(i));
-    }
-    const blob = new Blob([new Uint8Array(byteArrays)], {type: 'image/png'});
+function sendImageArrToService(imageArr) {
     const formData = new FormData();
-    formData.append('image', blob, 'image.png');
-
+    Array.from(imageArr).forEach((image, index) => {
+        formData.append(`image_${index}`, imageToFile(image));
+    });
 
     fetch('http://127.0.0.1:5000/upload', {
         method: 'POST',
@@ -104,6 +63,32 @@ function sendImageToService(image) {
     }).catch(error => {
             console.error('Error:', error);
     });
+
+//    const imageFile = imageToFile(image)
+//    formData.append('image', imageFile, 'image.png');
+}
+
+function extractImageFiles(imageAlbumElement) {
+    const imgElements =  Array.from(imageAlbumElement.querySelectorAll("img"));
+    return imgElements.filter((imgElement) => imgElement.src.includes("whatsapp"));
+}
+
+function imageToFile(image){
+    const canvas = document.createElement('canvas');
+    const context = canvas.getContext('2d');
+    canvas.width = image.width;
+    canvas.height = image.height;
+    context.drawImage(image, 0, 0);
+
+    const dataURL = canvas.toDataURL('image/png');
+    const imageData = dataURL.replace(/^data:image\/(png|jpeg);base64,/, '');
+
+    const byteCharacters = atob(imageData);
+    const byteArrays = [];
+    for (let i = 0; i < byteCharacters.length; i++) {
+        byteArrays.push(byteCharacters.charCodeAt(i));
+    }
+    return new Blob([new Uint8Array(byteArrays)], {type: 'image/png'});
 }
 
 function createButtonElement(imageAlbumElement) {
@@ -131,3 +116,26 @@ function createButtonElement(imageAlbumElement) {
     return filterButton;
 }
 
+function downloadImage(image){
+    const link = document.createElement('a');
+    link.href = image.src;
+    link.download = 'image.jpg';
+    link.click();
+}
+
+function forwardImage(imageAlbumElement) {
+    const forward = imageAlbumElement.querySelector('[data-testid="forward-chat"]')
+    if (forward) {
+        console.log(forward)
+        forward.click();
+        setTimeout(() => {
+            //const popup = document.querySelector('[data-testid="chat-modal"]')
+            const element = document.querySelector('[data-testid="message-yourself-row"]');
+            element.click();
+        }, 1000)
+        setTimeout(() => {
+            const sendBtn = document.querySelector('[data-testid="send"]')
+            sendBtn.click();
+        }, 1000)
+    }
+}
